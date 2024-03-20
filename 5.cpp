71b3be1ca34f67f064485fd65ed417e57c1949e2
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-03-19 22:00:52 -0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     setlocale(LC_ALL, "Portuguese");
-    int n, aux, m=0;
-    cout<<"Insira n: ";
-    cin>>n;
-    int *v = new int[n];
+    int n, aux, m = 0;
+    cout << "Insira n: ";
+    cin >> n;
+    int* v = new int[n];
 
-    for(int i=0;i<n;i++){
-        cout<<endl<<i+1<<"º valor: ";
-        cin>>v[i];
+    for (int i = 0; i < n; i++) {
+        cout << endl << i + 1 << "º valor: ";
+        cin >> v[i];
     }
 
-    int counter[n];
-    for(int i=0;i<n;i++)
-        counter[i]=0; //zero em todo array
+    int* counter = new int[n];
+    for (int i = 0; i < n; i++)
+        counter[i] = 0; //zero em todo array
 
-    for(int i=0;i<n;i++){
-        aux=v[i];
-        for(int j=i;j<n;j++){
-            if(aux==v[j])
+    for (int i = 0; i < n; i++) {
+        aux = v[i];
+        for (int j = i; j < n; j++) {
+            if (aux == v[j])
                 counter[i]++;
         }
     }
 
-    aux=counter[0];
-    for(int i=0;i<n;i++){
-        if(counter[i]>aux){
-            aux=counter[i];
-            m=i;
+    aux = counter[0];
+    for (int i = 0; i < n; i++) {
+        if (counter[i] > aux) {
+            aux = counter[i];
+            m = i;
         }
     }
 
-    cout<<endl<<"Moda: "<<v[m];
+    for (int i = 0; i < n; i++) {
+        if (counter[i]==counter[m]) {
+            cout << endl << "Moda: " << v[i];
+        }
+    }
 }
