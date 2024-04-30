22bd1186a60e615c7e7fdc56ffcf61110864abb4
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-04-30 08:25:30 -0300

    Create DogTest.java

---
+++ DogTest.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+                Dog dog = new Dog("Spike");
+                System.out.println(dog.getName() + " says " + dog.speak());
+    }
+}
