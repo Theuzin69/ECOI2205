4ec39a476923f43663ae7e9e9b86bf0b2173cde0
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-05-03 10:10:13 -0300

    Update DogTest.java

--- DogTest.java
+++ DogTest.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Dog dog1 = new Dog("Spike");
-        System.out.println(dog1.getName() + " says " + dog1.speak());
-
         Labrador labrador1 = new Labrador("Bidu", "Black");
-        System.out.println(labrador1.getName() + " says " + labrador1.speak());
+        System.out.println(labrador1.getName() + ", " + labrador1.avgBreedWeight() + " lbs, says " + labrador1.speak());
 
         Yorkshire yorkshire1 = new Yorkshire("Mia");
-        System.out.println(yorkshire1.getName() + " says " + yorkshire1.speak());
+        System.out.println(yorkshire1.getName() + ", " + yorkshire1.avgBreedWeight() + " lbs, says " + yorkshire1.speak());
     }
 }
