ae93360e12bee883d87724a07f7e1ac39e8d8b2c
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-05-03 10:09:32 -0300

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -6,27 +6,28 @@
 //
 // ****************************************************************
 
-public class Dog {
-        protected String name;
+public abstract class Dog {
+    protected String name;
 // ------------------------------------------------------------
 // Constructor -- store name
 // ------------------------------------------------------------
-public Dog(String name)
-        {
+    public Dog(String name)
+    {
             this.name = name;
-        }
+    }
 // ------------------------------------------------------------
 // Returns the dog's name
 // ------------------------------------------------------------
-        public String getName()
-        {
+    public String getName()
+    {
             return name;
-        }
+    }
 // ------------------------------------------------------------
 // Returns a string with the dog's comments
 // ------------------------------------------------------------
-        public String speak()
-        {
+    public String speak()
+    {
             return "Woof";
-        }
     }
+    public abstract int avgBreedWeight();
+}
