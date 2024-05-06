c452edb6349bf0950c3b375c36ac2d154ccd49f4
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-05-06 06:58:41 -0300

    Create Essay.java

---
+++ Essay.java
@@ -0,0 +1,12 @@
+public class Essay extends TestQuestion {
+    private Integer blackLineNum;
+    public void readQuestion(Integer LinesNum, TestQuestion question) {
+
+    }
+    public String toString()
+    {
+        String returnString = "";
+
+        return returnString;
+    }
+}
