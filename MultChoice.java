eea2dff9c5ea17dcad233c587de6aa3dbe1722fd
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-05-06 06:58:26 -0300

    Create MultChoice.java

---
+++ MultChoice.java
@@ -0,0 +1,13 @@
+public class MultChoice extends TestQuestion {
+    private String[] choices;
+    private Integer choicesNum;
+    public void readQuestion(Integer LinesNum, TestQuestion question) {
+
+    }
+    public String toString()
+    {
+        String returnString = "";
+
+        return returnString;
+    }
+}
