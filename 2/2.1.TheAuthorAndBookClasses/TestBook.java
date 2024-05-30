13b239a2d39888cfefc9ef4240cec0703f8bcfa7
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2024-05-30 17:47:49 -0300

    Update TestBook.java

--- 2/2.1.TheAuthorAndBookClasses/TestBook.java
+++ 2/2.1.TheAuthorAndBookClasses/TestBook.java
@@ -1,1 +1,37 @@
-d
+public class TestBook {
+    public static void main(String[] args){
+        // Construct an author instance
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);  // Author's toString()
+
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99);  // Test Book's Constructor
+        System.out.println(dummyBook);  // Test Book's toString()
+
+        // Test Getters and Setters
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(28);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor());  // Author's toString()
+        System.out.println("Author's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author's email is: " + dummyBook.getAuthor().getEmail());
+
+        // Use an anonymous instance of Author to construct a Book instance
+        Book anotherBook = new Book("more Java",
+                new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
+        System.out.println(anotherBook);  // toString()
+
+        /*TRY
+        1. Printing the name and email of the author from a Book instance.*/
+        System.out.println("name is: "+anotherBook.getAuthor().getName());
+        System.out.println("email is: "+anotherBook.getAuthor().getEmail());
+
+        /*2. Introduce new methods called getAuthorName(), getAuthorEmail(), getAuthorGender() in the Book class to return the name,
+        email and gender of the author of the book.*/
+        System.out.println("name is: "+anotherBook.getAuthorName());
+        System.out.println("email is: "+anotherBook.getAuthorEmail());
+        System.out.println("gender is: "+anotherBook.getAuthorGender());
+    }
+}
+
